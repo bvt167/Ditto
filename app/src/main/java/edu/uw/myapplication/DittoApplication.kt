45a434eb6c846255545d3f82c1241af065cdfed2
Brandon Ta <bvt@uw.edu>
2021-05-23 21:12:15 -0700

    created packages and application

---
+++ app/src/main/java/edu/uw/myapplication/DittoApplication.kt
@@ -0,0 +1,10 @@
+package edu.uw.myapplication
+
+import android.app.Application
+
+class DittoApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
--- app/src/main/java/edu/uw/myapplication/activity/MainActivity.kt
+++ app/src/main/java/edu/uw/myapplication/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package edu.uw.myapplication
+package edu.uw.myapplication.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import edu.uw.myapplication.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
